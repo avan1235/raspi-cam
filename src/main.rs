c4a4363574d33f2f921877e5dffabbdf5d5dd6c8
Maciej Procyk <maciej.procyk@jetbrains.com>
2026-01-03 22:59:25 +0100

    use example of ndi-camera

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,100 @@
+use std::ops::{Deref, DerefMut};
 use std::time::Duration;
+use clap::Parser;
+use color_eyre::eyre::Context;
+use libcamera::camera_manager::CameraManager;
+use libcamera::*;
+use libcamera::camera::{Camera, CameraConfiguration, CameraConfigurationStatus};
+use libcamera::framebuffer::{AsFrameBuffer, FrameMetadataStatus};
+use libcamera::framebuffer_allocator::{FrameBuffer, FrameBufferAllocator};
+use libcamera::framebuffer_map::MemoryMappedFrameBuffer;
+use libcamera::geometry::Size;
+use libcamera::pixel_format::PixelFormat;
+use libcamera::request::{ReuseFlag};
+use libcamera::stream::{StreamConfigurationRef, StreamRole};
+use ndi::{FourCCVideoType, FrameFormatType, VideoData};
+use tracing_subscriber::fmt;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
+use crate::buffer::DoubleBuffer;
 
-use libcamera::{
-    camera::CameraConfigurationStatus,
-    camera_manager::CameraManager,
-    framebuffer::AsFrameBuffer,
-    framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
-    framebuffer_map::MemoryMappedFrameBuffer,
-    pixel_format::PixelFormat,
-    properties,
-    stream::StreamRole,
-};
-use yuv::{yuv420_to_rgb, YuvChromaSubsampling, YuvPlanarImageMut, YuvRange, YuvStandardMatrix};
+mod rgb;
+mod bgr;
+mod yuyv;
+mod mjpeg;
+mod buffer;
+mod ndi_sender;
 
-// YUYV pixel format (YUV 4:2:2)
-const PIXEL_FORMAT_YUYV: PixelFormat = PixelFormat::new(u32::from_le_bytes([b'Y', b'U', b'Y', b'V']), 0);
+#[derive(Debug, Clone, Parser)]
+#[command(version)]
+pub struct Flags {
+    #[arg(long, default_value_t = 1920)]
+    pub width: u32,
+    #[arg(long, default_value_t = 1080)]
+    pub height: u32,
+    #[arg(short, long, default_value_t = 60)]
+    pub fps: u32, // TODO: check for fps in stream configuration
+    #[arg(short, long)]
+    pub name: Option<String>,
+    #[arg(long)]
+    pub format: Option<String>,
+}
 
-fn main() {
-    let filename = std::env::args().nth(1).expect("Usage ./raspi-cam <filename.jpg>");
+fn main() -> color_eyre::Result<()> {
+    let stream_formats: Vec<Box<dyn CameraStream>> = vec![
+        Box::new(bgr::BgrStream),
+        Box::new(rgb::RgbStream),
+        Box::new(yuyv::YuyvStream),
+        Box::new(mjpeg::MjpegStream),
+    ];
 
-    let mgr = CameraManager::new().unwrap();
+    ndi::initialize()?;
+    let flags = Flags::parse();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
-    let cameras = mgr.cameras();
-
-    println!("Found {} cameras, using first one", cameras.len());
+    let camera_manager = CameraManager::new()?;
+    let cameras = camera_manager.cameras();
 
     let cam = cameras.get(0).expect("No cameras found");
 
-    println!(
+    tracing::info!(
         "Using camera: {}",
-        *cam.properties().get::<properties::Model>().unwrap()
+        *cam.properties().get::<properties::Model>()?
     );
 
-    let mut cam = cam.acquire().expect("Unable to acquire camera");
+    let mut cam = cam.acquire()?;
 
-    // This will generate default configuration for each specified role
-    let mut cfgs = cam.generate_configuration(&[StreamRole::ViewFinder]).unwrap();
+    let camera_stream = if let Some(format_name) = flags.format {
+        let format_name = format_name.to_lowercase();
+        stream_formats.into_iter().find_map(|stream| (stream.name() == format_name).then_some(()).and(stream.is_supported(&cam).map(|cfg| (stream, cfg))))
+    }else {
+        stream_formats.into_iter().find_map(|stream| stream.is_supported(&cam).map(|cfg| (stream, cfg)))
+    };
 
-    // Use YUYV format since MJPEG is not supported
-    cfgs.get_mut(0).unwrap().set_pixel_format(PIXEL_FORMAT_YUYV);
+    let Some((camera_stream, mut cfg)) = camera_stream else {
+        color_eyre::eyre::bail!("No supported stream format found");
+    };
 
-    println!("Generated config: {cfgs:#?}");
+    cfg.get_mut(0).unwrap().set_size(Size::new(flags.width, flags.height));
 
-    match cfgs.validate() {
-        CameraConfigurationStatus::Valid => println!("Camera configuration valid!"),
-        CameraConfigurationStatus::Adjusted => println!("Camera configuration was adjusted: {cfgs:#?}"),
-        CameraConfigurationStatus::Invalid => panic!("Error validating camera configuration"),
+    match cfg.validate() {
+        CameraConfigurationStatus::Adjusted => tracing::warn!("Camera configuration was adjusted after changing frame size: {cfg:#?}"),
+        CameraConfigurationStatus::Invalid => color_eyre::eyre::bail!("Error validating camera configuration after changing frame_size"),
+        _ => {}
     }
 
-    cam.configure(&mut cfgs).expect("Unable to configure camera");
+    cam.configure(&mut cfg).context("Unable to configure camera")?;
 
     let mut alloc = FrameBufferAllocator::new(&cam);
 
     // Allocate frame buffers for the stream
-    let cfg = cfgs.get(0).unwrap();
+    let cfg = cfg.get(0).unwrap();
     let stream = cfg.stream().unwrap();
-    let buffers = alloc.alloc(&stream).unwrap();
-    println!("Allocated {} buffers", buffers.len());
-
-    // Get the actual width and height from the configuration
-    let width = cfg.get_size().width as usize;
-    let height = cfg.get_size().height as usize;
-    println!("Capture size: {}x{}", width, height);
+    let buffers = alloc.alloc(&stream)?;
+    tracing::debug!("Allocated {} buffers", buffers.len());
 
     // Convert FrameBuffer to MemoryMappedFrameBuffer, which allows reading &[u8]
     let buffers = buffers
@@ -69,10 +103,11 @@ fn main() {
         .collect::<Vec<_>>();
 
     // Create capture requests and attach buffers
-    let mut reqs = buffers
+    let reqs = buffers
         .into_iter()
-        .map(|buf| {
-            let mut req = cam.create_request(None).unwrap();
+        .enumerate()
+        .map(|(i, buf)| {
+            let mut req = cam.create_request(Some(i as u64)).unwrap();
             req.add_buffer(&stream, buf).unwrap();
             req
         })
@@ -84,51 +119,103 @@ fn main() {
         tx.send(req).unwrap();
     });
 
-    cam.start(None).unwrap();
+    // TODO: Set `Control::FrameDuration()` here. Blocked on https://github.com/lit-robotics/libcamera-rs/issues/2
+    cam.start(None)?;
 
-    // Multiple requests can be queued at a time, but for this example we just want a single frame.
-    cam.queue_request(reqs.pop().unwrap()).map_err(|(_, e)| e).unwrap();
+    // Enqueue all requests to the camera
+    for req in reqs {
+        tracing::debug!("Request queued for execution: {req:#?}");
+        cam.queue_request(req).map_err(|(_, e)| e)?;
+    }
 
-    println!("Waiting for camera request execution");
-    // Allow a bit more time for first exposure/conversion to complete on slower cameras.
-    let req = rx.recv_timeout(Duration::from_secs(5)).expect("Camera request failed");
+    let ndi_sender = ndi_sender::NdiSender::new(cfg.get_size(), flags.fps)?;
 
-    println!("Camera request {req:?} completed!");
-    println!("Metadata: {:#?}", req.metadata());
+    let mut buffer = DoubleBuffer::new(cfg.get_size());
 
-    // Get framebuffer for our stream
-    let framebuffer: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
-    println!("FrameBuffer metadata: {:#?}", framebuffer.metadata());
+    let mut last_capture = std::time::Instant::now();
 
-    // YUYV format has one data plane
-    let planes = framebuffer.data();
-    let yuyv_data = planes.first().unwrap();
-    let bytes_used = framebuffer.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize;
+    loop {
+        let mut req = rx.recv_timeout(Duration::from_secs(10))?;
+        tracing::debug!("Took {:?} since last capture", last_capture.elapsed());
 
-    println!("Converting YUYV to RGB...");
+        let frame_data = {
+            let instant = std::time::Instant::now();
 
-    let mut yuv_planar_image_mut =
-        YuvPlanarImageMut::<u8>::alloc(width as u32, height as u32, YuvChromaSubsampling::Yuv420);
-    let yuv_planar_image = yuv_planar_image_mut.to_fixed();
+            tracing::debug!("Camera request {req:?} completed!");
+            tracing::trace!("Metadata: {:#?}", req.metadata());
 
-    let mut rgba = vec![0u8; width * height * 4];
-    let rgba_stride = width * 4;
+            let framebuffer: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
+            tracing::trace!("FrameBuffer metadata: {:#?}", framebuffer.metadata());
+            let frame_metadata_status = framebuffer.metadata().unwrap().status();
+            if frame_metadata_status != FrameMetadataStatus::Success {
+                tracing::error!("Frame metadata status: {:?}", frame_metadata_status);
+                req.reuse(ReuseFlag::REUSE_BUFFERS);
+                cam.queue_request(req).map_err(|(_, e)| e)?;
 
-    yuv420_to_rgb(
-        &yuv_planar_image,
-        &mut rgba,
-        rgba_stride as u32,
-        YuvRange::Limited,
-        YuvStandardMatrix::Bt601,
-    )
-        .unwrap();
+                continue;
+            }
+            let bytes_used = framebuffer.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize;
 
-    println!("Encoding JPEG...");
-    // Encode RGB data to JPEG using the image crate
-    let img = image::RgbaImage::from_raw(width as u32, height as u32, rgba)
-        .expect("Failed to create image from RGB data");
+            let planes = framebuffer.data();
+            tracing::trace!("Data Planes: {:?}", planes.len());
+            let frame_data = planes.get(0).unwrap();
+            tracing::debug!("Frame captured in {:?}", instant.elapsed());
 
-    img.save(&filename).expect("Failed to save JPEG");
+            &frame_data[..bytes_used]
+        };
 
-    println!("Written image to {}", &filename);
+        let frame_info = {
+            let instant = std::time::Instant::now();
+            // TODO: Can we run this conversion and sending to ndi in the background as well to reduce latency?
+            let frame_info = camera_stream.convert_frame(&cfg, frame_data, &mut buffer)?;
+            tracing::debug!("Converted to {:?} in {:?}", frame_info.video_type, instant.elapsed());
+
+            frame_info
+        };
+
+        req.reuse(ReuseFlag::REUSE_BUFFERS);
+        cam.queue_request(req).map_err(|(_, e)| e)?;
+
+        {
+            let instant = std::time::Instant::now();
+            ndi_sender.send(&mut buffer, &frame_info)?;
+            tracing::debug!("Sent to NDI in {:?}", instant.elapsed());
+        }
+
+        last_capture = std::time::Instant::now();
+        buffer.swap();
+    }
+}
+
+trait CameraStream {
+    fn name(&self) -> &'static str;
+
+    fn is_supported(&self, camera: &Camera) -> Option<CameraConfiguration>;
+
+    fn convert_frame(&self, configuration: &StreamConfigurationRef, data: &[u8], target_buffer: &mut [u8]) -> color_eyre::Result<FrameInfo>;
+}
+
+pub struct FrameInfo {
+    pub video_type: FourCCVideoType,
+    pub stride: u32,
+}
+
+fn supports_configuration(cam: &Camera, format: PixelFormat) -> Option<CameraConfiguration> {
+    let mut cfgs = cam.generate_configuration(&[StreamRole::VideoRecording])?;
+
+    cfgs.get_mut(0)?.set_pixel_format(format);
+
+    tracing::trace!("Generated config: {cfgs:#?}");
+
+    match cfgs.validate() {
+        CameraConfigurationStatus::Valid => tracing::debug!("Camera configuration {format} valid!"),
+        CameraConfigurationStatus::Adjusted => tracing::trace!("Camera configuration was adjusted: {cfgs:#?}"),
+        CameraConfigurationStatus::Invalid => tracing::trace!("Error validating camera configuration for {format}"),
+    }
+
+    if cfgs.get(0).unwrap().get_pixel_format() != format {
+        return None;
+    }
+
+    Some(cfgs)
 }
